5bd8850579f23b3e080bcc14cddd52cd0da7eac1
maant001 <71355873+maant001@users.noreply.github.com>
2022-10-15 20:13:01 +0200

    methods

--- app/src/main/java/ch/zhaw/Lab04_Game_of_life/Board.kt
+++ app/src/main/java/ch/zhaw/Lab04_Game_of_life/Board.kt
@@ -6,10 +6,47 @@ class Board constructor(var rows: Int, var columns: Int) {
     fun calculateNextGen(boardLayout : Array<Array<Cell>>, rows: Int, columns: Int ) {
         val futureGen = Array(rows) {Array(columns) {Cell()} }
 
-        for (i in 0 until rows) {
-            for (j in 0 until columns) {
-                
+        for (l in 0 until rows) {
+            for (m in 0 until columns) {
+                var aliveNeighbours = checkAliveNeighbours(rows, columns) - boardLayout[l][m].state
+                if ((boardLayout[l][m].state == 1) && (aliveNeighbours < 2)) {
+                    
+                }
+
             }
         }
     }
+
+    fun checkAliveNeighbours(l: Int, m: Int): Int {
+        var aliveNeighbours = 0
+        // checking neighbouring cells
+        aliveNeighbours += checkNeighbouringCell(l - 1, m - 1)
+        aliveNeighbours += checkNeighbouringCell(l - 1, m)
+        aliveNeighbours += checkNeighbouringCell(l - 1, m + 1)
+        aliveNeighbours += checkNeighbouringCell(l, m - 1)
+        aliveNeighbours += checkNeighbouringCell(l, m + 1)
+        aliveNeighbours += checkNeighbouringCell(l + 1, m - 1)
+        aliveNeighbours += checkNeighbouringCell(l + 1, m)
+        aliveNeighbours += checkNeighbouringCell(l + 1, m + 1)
+
+        return aliveNeighbours
+    }
+
+    fun checkNeighbouringCell(row: Int, column: Int) : Int {
+        // check board out of bounds
+        if (row < 0 || column < 0) {
+            return 0
+        }
+        if (row >= rows || column >= columns) {
+            return 0
+        }
+
+        // check state of niegh cell
+        if (boardLayout[row][column].state == 1) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
 }
