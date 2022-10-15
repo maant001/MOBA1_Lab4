8e0651a3eb9e7ba16a02e7be49d23b6b65f55aa0
maant001 <71355873+maant001@users.noreply.github.com>
2022-10-15 21:46:59 +0200

    99% done, small single glitches i dont know why

--- app/src/main/java/ch/zhaw/Lab04_Game_of_life/Board.kt
+++ app/src/main/java/ch/zhaw/Lab04_Game_of_life/Board.kt
@@ -3,7 +3,7 @@ package ch.zhaw.Lab04_Game_of_life
 class Board constructor(var rows: Int, var columns: Int) {
     private var boardLayout = Array(rows) {Array(columns) {Cell()} }
 
-    fun calculateNextGen(boardLayout : Array<Array<Cell>>, rows: Int, columns: Int ) {
+    fun calculateNextGen(boardLayout : Array<Array<Cell>>, rows: Int, columns: Int ): Array<Array<Cell>> {
         val futureGen = Array(rows) {Array(columns) {Cell()} }
 
         for (l in 0 until rows) {
@@ -12,18 +12,18 @@ class Board constructor(var rows: Int, var columns: Int) {
                 // TODO needed?
                 // subtracting
                 //var aliveNeighbours = checkAliveNeighbours(rows, columns) - boardLayout[l][m].state
+                var aliveNeighbours = checkAliveNeighbours(l, m)
 
-                var aliveNeighbours = checkAliveNeighbours(rows, columns)
 
                 // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
                 if ((boardLayout[l][m].state == 1) && (aliveNeighbours < 2)) {
                     futureGen[l][m].state = 0
-                }
+                } else
 
                 // Any live cell with more than three live neighbours dies, as if by overpopulation.
                 if ((boardLayout[l][m].state == 1) && (aliveNeighbours > 3)) {
                     futureGen[l][m].state = 0
-                }
+                } else
 
                 // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                 if ((boardLayout[l][m].state == 0) && (aliveNeighbours == 3)) {
@@ -34,9 +34,10 @@ class Board constructor(var rows: Int, var columns: Int) {
 
             }
         }
+        return futureGen
     }
 
-    fun checkAliveNeighbours(l: Int, m: Int): Int {
+    private fun checkAliveNeighbours(l: Int, m: Int): Int {
         var aliveNeighbours = 0
         // checking neighbouring cells
         aliveNeighbours += checkNeighbouringCell(l - 1, m - 1)
@@ -55,17 +56,21 @@ class Board constructor(var rows: Int, var columns: Int) {
         // check board out of bounds
         if (row < 0 || column < 0) {
             return 0
-        }
+        } else
         if (row >= rows || column >= columns) {
             return 0
-        }
+        } else
 
-        // check state of niegh cell
+        // check state of neigh cell
         if (boardLayout[row][column].state == 1) {
             return 1
         } else {
             return 0
         }
+    }
+
+    fun getBoard() : Array<Array<Cell>> {
+        return boardLayout
     }
 
 }
--- app/src/main/java/ch/zhaw/Lab04_Game_of_life/main.kt
+++ app/src/main/java/ch/zhaw/Lab04_Game_of_life/main.kt
@@ -1,6 +1,37 @@
 package ch.zhaw.Lab04_Game_of_life
 
 fun main (args: Array<String>) {
+    var rows = 7
+    var columns = 7
+
+    var board = Board(rows, columns)
+
+    println("Original State:")
+    for (i in 0 until board.rows) {
+        for (j in 0 until board.columns) {
+            if (board.getBoard()[i][j].state == 1) {
+                print("1")
+            } else {
+                print("0")
+            }
+            Thread.sleep(150)
+        }
+        println()
+    }
+
+    println("Next State:")
+    var futureBoard = board.calculateNextGen(board.getBoard(), rows, columns)
+    for (i in futureBoard.indices) {
+        for (j in 0 until futureBoard[i].size) {
+            if (futureBoard[i][j].state == 1) {
+                print("1")
+            } else {
+                print("0")
+            }
+            Thread.sleep(150)
+        }
+        println()
+    }
 
 
 }
